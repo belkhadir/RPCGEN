16f1d64d6aab803c1e2c421f41f7fb4c0acc5699
belkhadir <belkhadir.anas@gmail.com>
2019-01-26 17:04:05 +0100

    Initial Commit

---
+++ RPCGEN/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  RPCGEN
+//
+//  Created by xxx on 1/26/19.
+//  Copyright © 2019 xxx. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
